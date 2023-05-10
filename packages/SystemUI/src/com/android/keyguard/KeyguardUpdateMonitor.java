ef3b0afe89f318853bd7b7b1ef24168d2a36ca20
rk134 <me@rk134.cf>
2023-05-10 16:12:59 +0000

    Revert "[SQUASH]: Add Pocket Mode"
    * deep sleep bonked sucessfully
    
    This reverts commit ad2da75d1bb1bbdd576e177f1cddcdbec4245006.
    
    Signed-off-by: rk134 <me@rk134.cf>

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -115,8 +115,6 @@ import android.os.SystemClock;
 import android.os.Trace;
 import android.os.UserHandle;
 import android.os.UserManager;
-import android.pocket.IPocketCallback;
-import android.pocket.PocketManager;
 import android.provider.Settings;
 import android.service.dreams.IDreamManager;
 import android.telephony.CarrierConfigManager;
@@ -231,9 +229,6 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
     private static final int MSG_TIME_FORMAT_UPDATE = 344;
     private static final int MSG_REQUIRE_NFC_UNLOCK = 345;
     private static final int MSG_KEYGUARD_DISMISS_ANIMATION_FINISHED = 346;
-
-    // Additional messages should be 600+
-    private static final int MSG_POCKET_STATE_CHANGED = 600;
 
     /** Biometric authentication state: Not listening. */
     private static final int BIOMETRIC_STATE_STOPPED = 0;
@@ -428,27 +423,6 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
     }
 
     private final Handler mHandler;
-
-    private PocketManager mPocketManager;
-    private boolean mIsDeviceInPocket;
-    private final IPocketCallback mPocketCallback = new IPocketCallback.Stub() {
-        @Override
-        public void onStateChanged(boolean isDeviceInPocket, int reason) {
-            boolean wasInPocket = mIsDeviceInPocket;
-            if (reason == PocketManager.REASON_SENSOR) {
-                mIsDeviceInPocket = isDeviceInPocket;
-            } else {
-                mIsDeviceInPocket = false;
-            }
-            if (wasInPocket != mIsDeviceInPocket) {
-                mHandler.sendEmptyMessage(MSG_POCKET_STATE_CHANGED);
-            }
-        }
-    };
-
-    public boolean isPocketLockVisible(){
-        return mPocketManager.isPocketLockVisible();
-    }
 
     private final IBiometricEnabledOnKeyguardCallback mBiometricEnabledCallback =
             new IBiometricEnabledOnKeyguardCallback.Stub() {
@@ -2214,10 +2188,6 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
                     case MSG_KEYGUARD_DISMISS_ANIMATION_FINISHED:
                         handleKeyguardDismissAnimationFinished();
                         break;
-                    case MSG_POCKET_STATE_CHANGED:
-                        updateBiometricListeningState(BIOMETRIC_ACTION_UPDATE,
-                                FACE_AUTH_UPDATED_FP_AUTHENTICATED);
-                        break;
                     default:
                         super.handleMessage(msg);
                         break;
@@ -2276,11 +2246,6 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
         mTrustManager.registerTrustListener(this);
 
         setStrongAuthTracker(mStrongAuthTracker);
-
-        mPocketManager = (PocketManager) context.getSystemService(Context.POCKET_SERVICE);
-        if (mPocketManager != null) {
-            mPocketManager.addCallback(mPocketCallback);
-        }
 
         if (mFpm != null) {
             mFingerprintSensorProperties = mFpm.getSensorPropertiesInternal();
@@ -2715,7 +2680,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
         BiometricAuthenticated face = mUserFaceAuthenticated.get(getCurrentUser());
         return mAssistantVisible && mKeyguardOccluded
                 && !(face != null && face.mAuthenticated)
-                && !mUserHasTrust.get(getCurrentUser(), false) && !mIsDeviceInPocket;
+                && !mUserHasTrust.get(getCurrentUser(), false);
     }
 
     private boolean shouldTriggerActiveUnlockForAssistant() {
@@ -2781,7 +2746,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
         boolean shouldListen = shouldListenKeyguardState && shouldListenUserState
                 && shouldListenBouncerState && shouldListenUdfpsState
                 && shouldListenSideFpsState
-                && !isFingerprintLockedOut() && !mIsDeviceInPocket;
+                && !isFingerprintLockedOut();
         logListenerModelData(
                 new KeyguardFingerprintListenModel(
                     System.currentTimeMillis(),
--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -169,8 +169,6 @@ import com.android.server.pm.UserManagerService;
 import com.android.server.pm.dex.OdsignStatsLogger;
 import com.android.server.pm.dex.SystemServerDexLoadReporter;
 import com.android.server.pm.verify.domain.DomainVerificationService;
-import com.android.server.pocket.PocketService;
-import com.android.server.pocket.PocketBridgeService;
 import com.android.server.policy.AppOpsPolicy;
 import com.android.server.policy.PermissionPolicyService;
 import com.android.server.policy.PhoneWindowManager;
@@ -488,8 +486,6 @@ public final class SystemServer implements Dumpable {
 
     /** Start the IStats services. This is a blocking call and can take time. */
     private static native void startIStatsService();
-
-    public boolean safeMode = false;
 
     /**
      * Start the memtrack proxy service.
@@ -1693,10 +1689,7 @@ public final class SystemServer implements Dumpable {
 
         // Before things start rolling, be sure we have decided whether
         // we are in safe mode.
-
-        if(wm != null) {
-            safeMode = wm.detectSafeMode();
-        }
+        final boolean safeMode = wm.detectSafeMode();
         if (safeMode) {
             // If yes, immediately turn on the global setting for airplane mode.
             // Note that this does not send broadcasts at this stage because
@@ -2557,10 +2550,6 @@ public final class SystemServer implements Dumpable {
             mSystemServiceManager.startService(CrossProfileAppsService.class);
             t.traceEnd();
 
-            t.traceBegin("StartPocketService");
-            mSystemServiceManager.startService(PocketService.class);
-            t.traceEnd();
-
             t.traceBegin("StartPeopleService");
             mSystemServiceManager.startService(PeopleService.class);
             t.traceEnd();
@@ -2568,14 +2557,6 @@ public final class SystemServer implements Dumpable {
             t.traceBegin("StartMediaMetricsManager");
             mSystemServiceManager.startService(MediaMetricsManagerService.class);
             t.traceEnd();
-
-            if (!context.getResources().getString(
-                    com.android.internal.R.string.config_pocketBridgeSysfsInpocket).isEmpty()) {
-                t.traceBegin("StartPocketBridgeService");
-                mSystemServiceManager.startService(PocketBridgeService.class);
-                t.traceEnd();
-            }
-
         }
 
         t.traceBegin("StartMediaProjectionManager");
